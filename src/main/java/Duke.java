71b3cfac9c1d7be3f4755dee7d12199c194ad729
Jazhten Low <jazhten@gmail.com>
2020-08-26 12:40:23 +0800

    Add basic Duke echo reply functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,17 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello there General Kenobi! I'm\n" + logo);
+        System.out.println("How may I be at your pleasure today?");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. May the Force be with you.");
+                break;
+            }
+            System.out.println("\t" + input);
+        }
+
     }
 }
